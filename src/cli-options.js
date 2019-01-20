fc6437f1adcc44423bf2126267dca005e2670a4e
Alex J Burke <alex@alexjeffburke.com>
2019-01-20 16:33:29 +0100

    Switch to yargs.

--- src/cli-options.js
+++ src/cli-options.js
@@ -1,35 +1,29 @@
-'use strict';
-
 var debug = require('debug')('dont-break');
-function list(val) {
-  return val.split(',');
-}
 
-var program = require('commander');
-program
-  .option(
-    '-t, --top-downloads <n>',
-    'Fetch N most downloaded dependent modules, save and check',
-    parseInt
-  )
-  .option(
-    '-s, --top-starred <n>',
-    'Fetch N most starred dependent modules, save and check',
-    parseInt
-  )
-  .option(
-    '-d, --dep <name1,name2,name3>',
-    'Check if current code breaks given dependent project(s)',
-    list
-  )
-  .option(
-    '--timeout <N seconds>',
-    'Wait for N seconds when installing a package',
-    parseInt
-  )
-  .parse(process.argv);
+var argv = require('yargs')
+  .option('t', {
+    alias: 'top-downloads',
+    type: 'number',
+    describe: 'Fetch N most downloaded dependent modules, save and check'
+  })
+  .option('s', {
+    alias: 'top-starred',
+    type: 'number',
+    describe: 'Fetch N most starred dependent modules, save and check'
+  })
+  .option('d', {
+    alias: 'dep',
+    type: 'array',
+    describe: 'Check if current code breaks given dependent project(s)'
+  })
+  .option('timeout', {
+    alias: 'timeout',
+    type: 'number',
+    describe: 'Wait for N seconds when installing a package'
+  })
+  .argv;
 
 debug('command line options');
-debug(program);
+debug(argv);
 
-module.exports = program;
+module.exports = argv;
