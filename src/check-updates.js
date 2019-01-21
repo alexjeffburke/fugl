1a5a0eebe86054d82649406bf4013a2594005da2
Alex J Burke <alex@alexjeffburke.com>
2019-01-21 22:13:07 +0100

    Strip out update notifier.

--- src/check-updates.js
+++
@@ -1,7 +0,0 @@
-(function checkForUpdates() {
-  var thisPackage = require('../package.json');
-  require('update-notifier')({
-    packageName: thisPackage.name,
-    packageVersion: thisPackage.version
-  }).notify();
-})();
