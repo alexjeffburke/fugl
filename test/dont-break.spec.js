b0cf80cb2d0c1cc59698d517098fccea8e972709
Alex J Burke <alex@alexjeffburke.com>
2019-01-23 20:13:42 +0100

    Add a basic test.

---
+++ test/dont-break.spec.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+
+var dontBreak = require('../src/dont-break');
+
+describe('when supplied module', () => {
+  it('should have created the module folder', () => {
+    return dontBreak({
+      package: 'dont-break-foo',
+      folder: path.join(__dirname),
+      dep: ['https://github.com/bahmutov/dont-break-bar.git']
+    }).then(() => {
+      assert.ok(
+        fs.existsSync(
+          path.join(
+            __dirname,
+            'builds',
+            'https-github-com-bahmutov-dont-break-bar-git'
+          )
+        )
+      );
+    });
+  });
+});
