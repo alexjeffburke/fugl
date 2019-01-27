14dc20eec4df7dcf510120477b470b7c0fa32f7a
Alex J Burke <alex@alexjeffburke.com>
2019-01-27 08:44:56 +0100

    Split out the single dependent test code.

--- src/dont-break.js
+++ src/dont-break.js
@@ -8,10 +8,7 @@ var quote = require('quote');
 var chdir = require('chdir-promise');
 var mkdirp = require('mkdirp');
 var debug = require('debug')('dont-break');
-var isRepoUrl = require('./is-repo-url');
-
 var _ = require('lodash');
-
 var fs = require('fs-extra');
 var exists = require('fs').existsSync;
 
@@ -19,6 +16,8 @@ var npm = require('top-dependents');
 var stripComments = require('strip-json-comments');
 // write found dependencies into a hidden file
 var dontBreakFilename = './.dont-break.json';
+
+var testDependent = require('./test-dependent');
 
 var MOCHA_HTML_DOCUMENT = `<html>
   <head>
@@ -29,13 +28,6 @@ var MOCHA_HTML_DOCUMENT = `<html>
   </body>
 </html>
 `;
-var NAME_COMMAND_SEPARATOR = ':';
-var DEFAULT_INSTALL_COMMAND = 'npm install';
-var DEFAULT_TEST_COMMAND = 'npm test';
-var INSTALL_TIMEOUT_SECONDS = 2 * 60 * 1000; // 2 minutes
-
-var installDependent = require('./install-dependency');
-var runInFolder = require('./run-in-folder');
 
 function saveTopDependents(name, metric, n) {
   la(check.unemptyString(name), 'invalid package name', name);
@@ -168,153 +160,6 @@ function getDependents(options) {
   }
 
   return firstStep.then(() => getDependentsFromFile(options));
-}
-
-function testInFolder(emitter, dependent, testCommand, folder) {
-  const test = {
-    title: dependent.name,
-    body: '',
-    duration: 0,
-    fullTitle: () => dependent.name,
-    slow: () => 0
-  };
-
-  return runInFolder(folder, testCommand, {
-    missing: 'missing test command',
-    success: () => emitter.emit('pass', test),
-    failure: err => {
-      emitter.emit('fail', test, err);
-    }
-  });
-}
-
-function getDependencyName(dependent) {
-  if (isRepoUrl(dependent)) {
-    debug('dependent is git repo url %s', dependent);
-    return dependent;
-  }
-  const nameParts = dependent.split(NAME_COMMAND_SEPARATOR);
-  la(nameParts.length, 'expected at least module name', dependent);
-  const moduleName = nameParts[0].trim();
-  return moduleName;
-}
-
-function testDependent(emitter, options, dependent, config) {
-  if (check.string(dependent)) {
-    dependent = { name: dependent.trim() };
-  }
-  dependent = Object.assign({ pretest: true }, config, dependent);
-
-  var moduleInstallCommand = dependent.install || DEFAULT_INSTALL_COMMAND;
-  var modulePostinstallCommand =
-    dependent.postinstall || `npm install ${options.packageName}`;
-  var moduleTestCommand = dependent.test || DEFAULT_TEST_COMMAND;
-
-  const moduleName = getDependencyName(dependent.name);
-
-  la(check.unemptyString(moduleName), 'invalid dependent', moduleName);
-
-  function formFullFolderName() {
-    if (isRepoUrl(moduleName)) {
-      // simple repo installation
-      return toFolder;
-    } else {
-      let scoped = moduleName.startsWith('@');
-      let idx = scoped ? 1 : 0;
-      let moduleDir = moduleName.split('@')[idx];
-      moduleDir = scoped ? `@${moduleDir}` : moduleDir;
-      return path.join(toFolder, 'node_modules', moduleDir);
-    }
-  }
-
-  var cwd = process.cwd();
-  process.env.CURRENT_MODULE_NAME = moduleName;
-  process.env.CURRENT_MODULE_DIR = cwd;
-
-  function expandCommandVars(command) {
-    if (!command) {
-      return command;
-    }
-    command = command.replace('$CURRENT_MODULE_DIR', cwd);
-    command = command.replace('$CURRENT_MODULE_NAME', moduleName);
-    return command;
-  }
-
-  function postInstallModuleInFolder(dependentFolder) {
-    var command = expandCommandVars(modulePostinstallCommand);
-    return runInFolder(dependentFolder, command, {
-      success: 'postinstall succeeded',
-      failure: 'postinstall did not work'
-    });
-  }
-
-  var safeName = _.kebabCase(_.deburr(moduleName));
-  debug('original name "%s", safe "%s"', moduleName, safeName);
-  var toFolder = path.join(options.tmpDir, safeName);
-  debug('testing folder %s', quote(toFolder));
-
-  var timeoutSeconds = options.timeout || INSTALL_TIMEOUT_SECONDS;
-  la(
-    check.positiveNumber(timeoutSeconds),
-    'wrong timeout',
-    timeoutSeconds,
-    options
-  );
-
-  var installOptions = {
-    moduleName,
-    toFolder,
-    cmd: expandCommandVars(moduleInstallCommand)
-  };
-
-  var res = Promise.race([
-    installDependent(installOptions),
-    new Promise(resolve =>
-      setTimeout(() => resolve({ timeout: true }), timeoutSeconds)
-    )
-  ])
-    .then(result => {
-      if (result && result.timeout) {
-        debug('install timed out for ' + moduleName);
-        throw new Error('timeout');
-      }
-      return result;
-    })
-    .then(formFullFolderName)
-    .then(function checkInstalledFolder(folder) {
-      la(check.unemptyString(folder), 'expected folder', folder);
-      la(exists(folder), 'expected folder to exist', folder);
-      return folder;
-    });
-
-  var testWithPreviousVersion = dependent.pretest;
-  if (testWithPreviousVersion) {
-    var modulePretestCommand;
-    if (check.type('string', testWithPreviousVersion)) {
-      modulePretestCommand = testWithPreviousVersion;
-    } else {
-      modulePretestCommand = moduleTestCommand;
-    }
-    res = res.then(postInstallModuleInFolder).then(folder => {
-      return testInFolder(emitter, dependent, modulePretestCommand, folder);
-    });
-  }
-
-  return res
-    .then(postInstallModuleInFolder)
-    .then(folder => {
-      return testInFolder(emitter, dependent, moduleTestCommand, folder);
-    })
-    .catch(err => {
-      emitter.emit('fail', {
-        title: dependent.name,
-        err
-      });
-    })
-    .then(function() {
-      debug('restoring original directory', cwd);
-      process.chdir(cwd);
-    });
 }
 
 function testDependents(options, config) {
